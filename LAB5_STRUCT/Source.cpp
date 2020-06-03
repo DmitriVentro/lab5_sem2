615dd1d0708c8e88ed8a0558d913e7f60063cd40
dmitrithedwarf <dmitriventro@gmail.com>
2020-06-03 18:24:08 +0700

    final_cmm

--- LAB5_STRUCT/Source.cpp
+++ LAB5_STRUCT/Source.cpp
@@ -6,13 +6,24 @@
 using namespace std;
 struct SOLD_AIR_TICKET
 {
-	
 	string FLICHT_NUMBER = " "; 
 	string SNP = " ";
 	string DESTINATION = " ";
-	string DURATION_IN_MINUTES = " ";
+	int DURATION_IN_MINUTES = 0;
 };
-void SHOW_ALL_TICKETS(int n, SOLD_AIR_TICKET* ARRAY);//show all tickets in your file
+//Upload information about all tickets sold and display.
+void SHOW_ALL_TICKETS(int n, SOLD_AIR_TICKET* ARRAY);
+//Find out on which flights passengers bought tickets by the name of Sidorov.
+void SEARCH_SURNAME(int n, SOLD_AIR_TICKET* ARRAY);
+/*Display information on all tickets for flights lasting more than three hours.
+Data must be sorted by passenger's last name.*/
+void SORT_N_MINUTES(int n, SOLD_AIR_TICKET* ARRAY);
+
+bool cmpByAuthor(const SOLD_AIR_TICKET& r1, const SOLD_AIR_TICKET& r2) //сравнение по фамилии автора
+{
+	return r1.SNP < r2.SNP;
+}
+
 int main()
 {
 	setlocale(LC_ALL, " ");
@@ -39,7 +50,11 @@ int main()
 		iss >> PART.FLICHT_NUMBER >> PART.SNP >> PART.DESTINATION >> PART.DURATION_IN_MINUTES;
 		ARRAY[i] = PART;
 	}
-
+	cout << "1. Upload information about all tickets sold and display.\n"
+		 << "2. Find out on which flights passengers bought tickets by the name of Sidorov.\n"
+		 << "3. Display information on all tickets for flights lasting more than three hours.\n"
+		 << "Data must be sorted by passenger's last name\n"
+		 <<">>>";
 	int choice;
 	cin >> choice;
 	switch (choice)
@@ -48,7 +63,12 @@ int main()
 		SHOW_ALL_TICKETS(n, ARRAY);
 		break;
 	case 2:
-		cout << "suck:)";
+		//Find out on which flights passengers bought tickets by the name of Sidorov.
+		SEARCH_SURNAME(n, ARRAY);
+	case 3:
+		/*Display information on all tickets for flights lasting more than three hours.
+		Data must be sorted by passenger's last name.*/
+		SORT_N_MINUTES(n, ARRAY);
 	default:
 		break;
 	}
@@ -60,13 +80,51 @@ int main()
 void SHOW_ALL_TICKETS(int n, SOLD_AIR_TICKET* ARRAY)
 {
 	{
-		cout << "Flight number/Initials Of The Passenger/Destination/Duration In Minutes\n\n";
+		cout << "Flight number/Initials Of The Passenger/Destination/Duration In Minutes:\n\n";
 		for (size_t i = 0; i < n; i++)
 		{
-			cout << " " << ARRAY[i].FLICHT_NUMBER << " "
-				 << ARRAY[i].SNP << " "
-				 << ARRAY[i].DESTINATION << " "
+			cout << " " << ARRAY[i].FLICHT_NUMBER 
+				 << " "
+				 << ARRAY[i].SNP
+				 << " "
+				 << ARRAY[i].DESTINATION
+				 << " "
 				 << ARRAY[i].DURATION_IN_MINUTES;
+			cout << "\n\n";
+		}
+	}
+}
+
+void SEARCH_SURNAME(int n, SOLD_AIR_TICKET* ARRAY)
+{
+	for (size_t i = 0; i < n; i++)
+	{
+		if (!(ARRAY[i].SNP.find("SIDOROV")))
+		{
+			cout << " " << ARRAY[i].FLICHT_NUMBER 
+				 << " "
+				 << ARRAY[i].SNP
+				 << " "
+				 << ARRAY[i].DESTINATION
+				 << " "
+				 << ARRAY[i].DURATION_IN_MINUTES;
+			cout << "\n\n";
+		}
+	}
+}
+void SORT_N_MINUTES(int n, SOLD_AIR_TICKET* ARRAY)
+{	
+	
+	sort(ARRAY, ARRAY + n, cmpByAuthor);
+	for (size_t i = 0; i < n; i++)
+	{
+		if (ARRAY[i].DURATION_IN_MINUTES > 180)
+		{
+			cout << " " << ARRAY[i].FLICHT_NUMBER
+				 << " "
+				 << ARRAY[i].SNP
+				 << " "
+				 << ARRAY[i].DESTINATION;
 			cout << "\n\n";
 		}
 	}
